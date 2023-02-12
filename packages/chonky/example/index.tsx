f5124bbdfd6f8f4c303ad3bc1d74f888b5dfb2ff
Christian Stewart <christian@paral.in>
2023-02-12 02:46:05 -0800

    release: 0.2.1
    Signed-off-by: Christian Stewart <christian@paral.in>

--- packages/chonky/example/index.tsx
+++ packages/chonky/example/index.tsx
@@ -1,7 +1,6 @@
-import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { FullFileBrowser } from '../.';
+import { FullFileBrowser } from '../dist/index.esm';
 
 const App = () => {
   return (
